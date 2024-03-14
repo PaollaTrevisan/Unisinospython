8bde319971a142afc356e92ee87d79600617b5d6
Paolla Trevisan <153026926+PaollaTrevisan@users.noreply.github.com>
2024-03-14 12:37:18 -0300

    Feito

--- ex3.py
+++ ex3.py
@@ -6,4 +6,14 @@ v = v1/v2
 vr = float("%.2f" % v)
 print(vr)
 #solicitar ao usuário 2 valores com ponto flutuante e imprimir na tela o resultado da divisão de um pelo outro com 3 casas decimais
+valor1 = float(input("Digite o valor"))
+valor2 = float(input("Digite outro valor"))
+divisao = valor1/valor2
+nvalor = float("%.3f" % divisao)
+print(nvalor)
 #solicitar ao usuário 2 valores com ponto flutuante e imprimir na tela o resultado da divisão de um pelo outro com 4 casas decimais
+num1 = float(input("Digite um número: "))
+num2 = float(input("Digite outro número: "))
+dnum = num1/num2
+res = float("%.4f" % dnum)
+print(res)
